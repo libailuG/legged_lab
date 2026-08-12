39b2a6473b5c22782c9a5a4c8a688c6903c7c08c
lugangtai <libai_lu@163.com>
2026-08-12 09:15:51 +0800

    提交修改

--- libai_cmd.py
+++ libai_cmd.py
@@ -1,10 +1,10 @@
 '''
 
 
+python scripts/rsl_rl/train.py --task LeggedLab-Isaac-AMP-G1-v0 --headless --max_iterations 50000
 
 
-
-
+python scripts/rsl_rl/train.py --task LeggedLab-Isaac-AMP-G1-v0 --num_envs 1
 
 
 '''
